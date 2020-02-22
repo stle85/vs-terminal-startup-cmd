b75ae70f7fcbfe392b9c05413d823f89093884a1
Stanislav Lebedev <kugui@yandex.ru>
2020-02-22 22:52:14 +0400

    * platform base settings have been added * extension code has been rewritten * documentation has been added

--- README.md
+++ README.md
@@ -2,17 +2,27 @@
 
 ## Overview
 
-The VS Code plugin provides the ability to run a command when opening a terminal.
+The VS Code plug-in provides the ability to run a command when opening a terminal.
 
 ## Settings
 
+There are several settings which are provided by the extension.
+
+| name                        | description                                                                                    |
+| --------------------------- | ---------------------------------------------------------------------------------------------- |
+| terminal.startupCmd         | The command which will be triggered when the terminal opened. Override other startup settings. |
+| terminal.startupCmd.linux   | The command which will be triggered when the terminal opened on *nix platform.                 |
+| terminal.startupCmd.osx     | The command which will be triggered when the terminal opened on Mac OS platform.               |
+| terminal.startupCmd.windows | The command which will be triggered when the terminal opened on Windows platform.              |
+
+An example.
 ```json
 {
   "terminal.startupCmd": "source ${workspaceFolder}/work.env"
 }
 ```
 
-## Running the plugin
+## Running the plug-in
 
 - Run `npm install` in terminal to install dependencies
 - Run the `Run Extension` target in the Debug View. This will:
--- src/extension.ts
+++ src/extension.ts
@@ -2,24 +2,83 @@
 
 import {
 	window,
-	workspace, 
-	ExtensionContext, 
+	workspace,
+	ExtensionContext,
 	Terminal,
+	WorkspaceConfiguration,
 } from 'vscode';
 
 
-export function activate(context: ExtensionContext) {
-	const configName = 'terminal.startupCmd';
-
-	let workspaceFolder: string = '';
+/**
+ * Get the current workspace folder path.
+ * 
+ * @returns - The current workspace folder path.
+ */
+function getWorkspaceFolder(): string {
 	if (workspace.workspaceFolders) {
-		workspaceFolder = workspace.workspaceFolders[0].uri.path;
+		return workspace.workspaceFolders[0].uri.path;
 	}
+	return '';
+}
 
-	const cmd: string | undefined = workspace.getConfiguration().get(configName);
+/**
+ * Get the current platform name.
+ * 
+ * @returns - The current platform name.
+ */
+function getPlatform(): string {
+	switch (process.platform) {
+		case 'darwin':
+			return 'osx';
+
+		case 'win32':
+			return 'windows';
+
+		default:
+			return 'linux';
+	}
+}
+
+/**
+ * Get the cmd for running from settings.
+ * 
+ * @returns - The cmd from settings.
+ */
+function getCmd(): string | undefined {
+	const configName = 'terminal.startupCmd';
+	const platformConfigName = `terminal.startupCmd.${getPlatform()}`;
+
+	const configuration: WorkspaceConfiguration = workspace.getConfiguration();
+	const platformCmd: string | undefined = configuration.get(platformConfigName);
+	if (platformCmd) {
+		return platformCmd;
+	}
+	return configuration.get(configName);
+}
+
+/**
+ * Expand variables in the string.
+ * 
+ * @param str - The string with variables.
+ * @returns - The expanded string.
+ */
+function expandVars(str: string): string {
+	const workspaceFolder: string = getWorkspaceFolder();
+	return str.replace('${workspaceFolder}', workspaceFolder)
+}
+
+
+/**
+ * The extension's activate function.
+ * 
+ * @param context - The extension context.
+ */
+export function activate(context: ExtensionContext) {
 	window.onDidOpenTerminal((terminal: Terminal) => {
-		if (cmd) {
-			terminal.sendText(cmd.replace('${workspaceFolder}', workspaceFolder));
+		const cmd: string | undefined = getCmd();
+		if (cmd && '' !== cmd) {
+			const preparedCmd: string = expandVars(cmd);
+			terminal.sendText(preparedCmd);
 		}
 	});
 }
